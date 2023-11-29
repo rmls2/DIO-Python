c8b078a0ca4b492572ba632510739d4a94a78c02
rmls2 <millerrobert182@gmail.com>
2023-11-29 14:56:09 -0300

    add mais exemplos em propriedades_ex1

--- poo/propriedades_ex1.py
+++ poo/propriedades_ex1.py
@@ -27,3 +27,48 @@ foo.x = 10
 print(foo.x)
 
 print('esse é o valor de x agora', foo.x)
+
+# exemplo de uso de decorator, fora do contexto de classe.:
+
+def decorator(funcao):
+    def wrapper():
+        print ("Estou antes da execução da função passada como argumento")
+        funcao()
+        print ("Estou depois da execução da função passada como argumento")
+
+    return wrapper
+
+def outra_funcao():
+    print ("Sou um belo argumento!")
+
+funcao_decorada = decorator(outra_funcao)
+funcao_decorada()
+
+## outro exemplo:
+
+import time
+
+# Define nosso decorator
+def calcula_duracao(funcao):
+    def wrapper():
+        # Calcula o tempo de execução
+        tempo_inicial = time.time()
+        funcao()
+        tempo_final = time.time()
+
+        # Formata a mensagem que será mostrada na tela
+        print("[{funcao}] Tempo total de execução: {tempo_total}".format(
+            funcao=funcao.__name__.upper(),
+            tempo_total=str(tempo_final - tempo_inicial))
+        )
+
+    return wrapper
+
+# Decora a função com o decorator
+@calcula_duracao
+def main():
+    for n in range(0, 10000000):
+        pass
+
+# Executa a função main
+main()
