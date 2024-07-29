71eb7e03017c0ddaec6ea0694994af3db9ac73d6
Robert Miller <robert.miller@ufpe.br>
2024-07-28 23:19:41 -0300

    script para entender o funcionamento de pedaços de código, funções, classes, etc

---
+++ teste_de_codigos_e_func.py
@@ -0,0 +1,12 @@
+# def elementos_comuns(lista_1, lista_2):
+#     set_1 = set(map(int, lista_1.split()))
+#     set_2 = set(map(int, lista_2.split()))
+#
+#     return list(set_1.intersection(set_2))
+#
+#
+# x = elementos_comuns('1 2 3 4', '3 4 5 6')
+# print(x)
+
+# verifica se uma string pode ser convetida para um número
+print('a'.isdigit())
